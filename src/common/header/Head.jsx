b9a5a93ffc6229f5b90a3131621f4e20d99b8341
Matheusmalta1 <matheusmaltaa@hotmail.com>
2023-07-16 13:52:11 -0300

    ajustes do head

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,13 +1,15 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Head = () => {
   return (
     <section className='head'>
       <div className='container d_flex'>
         <div className='left-row'>
-          <i className='fa fa-phone'></i>
+          <FontAwesomeIcon icon={faPhone} />          
           <label> +88012 3456 7894</label>
-          <i className='fa fa-envelope'></i>
+          <FontAwesomeIcon icon={faEnvelope} />
           <label> lil_pizzarie@vamonessa.com</label>
         </div>
         <div className='right-row RText'>
