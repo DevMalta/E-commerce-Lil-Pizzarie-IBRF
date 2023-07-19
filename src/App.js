ddd04bb1f06805a81ed59ccbeeb851d5f9903e13
alex bernardino <oalexbernardino@gmail.com>
2023-07-18 22:32:09 -0300

    changes test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Menu from './Components/Menu';
 import Footer from "./common/footer/Footer"
 import Header from "./common/header/Header"
 import Login from './Components/Login';
-import Cart from "./Components/componentes_Menu/Cart";
+// import Cart from "./Components/componentes_Menu/Cart";
 import Teste from "./Components/Teste";
 import './App.css';
 
@@ -22,7 +22,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="/cart" element={<Cart />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/teste" element={<Teste />} />
         {/* <Route path="/cart" exact element={<Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} />} /> */}
