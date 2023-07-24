f7e64cd8c3a632a7eab4b22d573f3cc98c2910b0
Matheusmalta1 <matheusmaltaa@hotmail.com>
2023-07-23 21:49:52 -0300

    cart page em processo

--- src/Components/componentes_Menu/CartPage.jsx
+++ src/Components/componentes_Menu/CartPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import Cart from './Cart';
+// import  from '../Teste';
+import handleQuantityDecrement from '../Teste';
+import handleQuantityIncrement from '../Teste';
+import quantidades from '../Teste';
+
+
+
+
 
 function CartPage() {
+    
+
   return (
-    <div>
-      <h1>Meu Componente</h1>
-      <p>Este é um componente React básico.</p>
+    <div className="Página">
+         <Cart /> 
     </div>
-  );
+  )
 }
 
 export default CartPage;
