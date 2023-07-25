fae64a02efa754730c0ba7d82bf38b6380a9e74e
Matheusmalta1 <matheusmaltaa@hotmail.com>
2023-07-24 23:07:26 -0300

    mudança de arquivo-type

---
+++ src/object/Modal.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import StarRating from './StarRating';
+
+const PizzaModal = ({ show, onHide, pizza }) => {
+  // Verificar se a pizza está definida antes de acessar suas propriedades
+  if (!pizza) return null;
+
+  const modalStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
+  const imageStyle = {
+    maxWidth: '80%',
+    height: 'auto',
+  };
+
+  // Defina a função handleRatingChange para lidar com as alterações na classificação por estrelas
+  const handleRatingChange = (newRating) => {
+    // Faça algo com o valor newRating (por exemplo, atualize a classificação da pizza)
+    console.log('Nova classificação:', newRating);
+  };
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{pizza.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div style={modalStyle}> {/* Aplicar o estilo diretamente na div */}
+          <img
+            src={pizza.src}
+            alt={pizza.name}
+            style={imageStyle} 
+          />
+          <p>{pizza.description}</p>
+          <p>Preço: {pizza.price}</p>
+
+          {/* Aqui estão os componentes StarRating */}
+          <div className="product-rating">
+            <StarRating initialValue={pizza.rating} onChange={(newRating) => handleRatingChange(newRating)} />
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Fechar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default PizzaModal;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Home.css'; // Importe o arquivo CSS para estilização
+import pizza from '../img/pizza_home.jpeg';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Bem-vindo à Lil Pizzarie</h1>
+      <p className="home-description">Lil Pizzarie: o lugar onde cada fatia é uma obra de arte, e você é o artista que saboreia!</p>
+      <img src={pizza} alt="Pizza" className="home-image" />
+      <p className="home-text">Confira o nosso cardápio e faça seu pedido agora mesmo.</p>
+    </div>
+  );
+}
+
+export default Home;
