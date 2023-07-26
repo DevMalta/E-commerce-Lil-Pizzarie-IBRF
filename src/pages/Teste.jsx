2d6f52601da816cc030b36549cc2f2b4fe7aeb2a
Matheusmalta1 <matheusmaltaa@hotmail.com>
2023-07-26 00:54:38 -0300

    att

---
+++ src/pages/Teste.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/AboutUs.css'; // Importe o arquivo CSS para estilização
+import forno from '../img/forno.jpeg';
+
+
+const Teste = () => {
+  return (
+    <div className="about-us-container">
+        <h1>Teste</h1>
+    </div>
+  );
+};
+
+export default Teste;
+
