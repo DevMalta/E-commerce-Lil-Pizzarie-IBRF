2c15c494b431bba7371710a73b7dfc9c0712c533
alex bernardino <oalexbernardino@gmail.com>
2023-08-03 15:59:04 -0300

    ajustes finais parte 2

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import './Navbar.css';
-
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
@@ -18,22 +15,21 @@ const NavBar = () => {
           <div className="menu-btn-burger"></div>
         </div>
         <ul className={`menu ${menuOpen ? 'active' : ''}`}>
-
-            <li>
-              <Link to="/menu">
-                Menu
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">
-                Sobre Nós
-              </Link>
-            </li>
-            <li>
-              <Link to="/contato">
-                Contato
-              </Link>
-            </li>
+          <li>
+            <Link to="/about">
+              Sobre Nós
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/contato">
+              Contato
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import '../styles/AboutUs.css'; // Importe o arquivo CSS para estilização
-import dev1 from '../img/alex.jpeg';
+import '../styles/AboutUs.css';import dev1 from '../img/alex.jpeg';
 import dev2 from '../img/dev-matheus.jpeg';
-// import dev3 from '../img/dev-fabio.jpeg';
+import dev3 from '../img/dev-fabio.jpeg';
 import dev4 from '../img/dev-hugo.png';
 import dev5 from '../img/dev-icaro.png';
-// import dev6 from '../img/dev-leandro.png';
+import dev6 from '../img/dev-leandro.png';
 import prof from '../img/prof-josafa.jpeg';
 import ybelt from '../img/yellow-belt.jpeg';
 import logo1 from '../img/logo-brf.png';
@@ -23,6 +22,9 @@ import iCafe from '../img/iCafe.png';
 import iReact from '../img/iReact.png';
 import iMeet from '../img/iMeet.png';
 import iGpt from '../img/iGpt.png';
+import iFont from '../img/iFont.webp';
+import iPer from '../img/iPer.webp';
+import iW3 from '../img/iW3.svg';
 
 
 const AboutUs = () => {
@@ -32,26 +34,29 @@ const AboutUs = () => {
       <hr />
       <div className="about-us-container">        
         <div className='project-container'>
-         <div >
-            <h2>Ferramentas</h2>
-            <tr className='obj-tools'>
-              <ul className='tools'>
-                <li><img src={iVscode} className='icone'/>{' VSCode'} </li >
-                <li><img src={iNode} className='icone'/>{' Node'} </li >
-                <li><img src={iReact} className='icone'/>{' React'} </li >
-                <li><img src={iBootstrap} className='icone'/>{' Bootstrap'} </li >
-                <li><img src={iGpt} className='icone'/>{' ChatGPT'} </li >
-                <li><img src={iTrello} className='icone'/>{' Trello'} </li >
-                <li><img src={iMeet} className='icone'/>{'iMeet'} </li >
-                <li><img src={iDiscord} className='icone'/>{' Discord'} </li >
-                <li><img src={iGit} className="icone" />{' GitHub'} </li >
-                <li><img src={iCafe} className='icone'/>{' Muito café'} </li >
-              </ul>
-            </tr>
+
+        <div className='objetivos-container'>
+            <div className='objetivos'>
+              <h5><strong>Objetivos do Projeto</strong></h5>
+              <p>O objetivo de aprendizagem
+                para o desenvolvimento front-end de um e-commerce é
+                adquirir as habilidades e conhecimentos necessários para
+                criar interfaces de usuário atraentes, funcionais e
+                responsivas para um site de comércio eletrônico.</p>
+            </div>
+            <div className='objetivos'>
+            <h5><strong>Objetivos do FrioLil's</strong></h5>
+            <p>O objetivo do FrioLil's foi construir um e-commerce moderno, eficiente e
+              escalável, que atenda às demandas dos usuários, garantindo uma
+              experiência ágil e responsiva, e que principalmente, trouxesse uma
+              nova tela para as vendas dos produtos dos nossos patrocinadores</p>
+            </div>
           </div>
+
           <hr />
+
           <div className='devs'>
-            <h2>Equipe de Desenvolvimento:</h2>
+            <h2>Colaboradores:</h2>
           <div className='dev-container'>
             <div className='group-dev'>
               <div class="circle">
@@ -73,14 +78,6 @@ const AboutUs = () => {
                     Dev. Master</p>
               </div>
             </div>
-            {/* <div className='group-off'>
-              <div class="circle">
-                <img src={dev3} alt="Dev-Fabio" />
-              </div>
-              <div className='dev-desc'>
-                <h4>Dev. Fabio</h4>
-              </div>
-            </div> */}
             <div className='group-dev'>
                 <div class="circle">
                   <img src={prof} alt="Josafá" />
@@ -105,7 +102,7 @@ const AboutUs = () => {
                 <img src={dev4} alt="Dev-Hugo" />
               </div>
               <div className='dev-desc'>
-                <h4>Dev. Hugo</h4>
+                <h4>Hugo</h4>
               </div>
             </div>
             <div className='group-off'>
@@ -113,43 +110,81 @@ const AboutUs = () => {
                 <img src={dev5} alt="Dev-Icaro" />
               </div>
               <div className='dev-desc'>
-                <h4>Dev. Icaro</h4>
+                <h4>Icaro</h4>
               </div>
             </div>
-            {/* <div className='group-off'>
+            <div className='group-off'>
               <div class="circle">
                 <img src={dev6} alt="Dev-Leandro" />
               </div>
               <div className='dev-desc'>
-                <h4>Dev. Leandro</h4>
+                <h4>Leandro</h4>
               </div>
-            </div> */}
+            </div>
+            <div className='group-off'>
+              <div class="circle">
+                <img src={dev3} alt="Dev-Fabio" />
+              </div>
+              <div className='dev-desc'>
+                <h4>Fabio</h4>
+              </div>
+            </div>
+            
           </div>
             
           </div>
+
           <hr />
-          <div className='objetivos-container'>
-            <div className='objetivos'>
-              <h5><strong>Objetivos do Projeto</strong></h5>
-              <p>O objetivo de aprendizagem
-                para o desenvolvimento front-end de um e-commerce é
-                adquirir as habilidades e conhecimentos necessários para
-                criar interfaces de usuário atraentes, funcionais e
-                responsivas para um site de comércio eletrônico.</p>
+
+         <div className='func-container'>
+         <div className='functions'>
+              <h2>Funcionalidades</h2>
+              <tr className='tr-functions'>
+                <ul className="ul-functions">
+                  <li>Lista de compras fixa na tela</li>
+                  <li>Telas com responsividade</li>
+                  <li>Tela de pagamentos dinamica</li>
+                  <li>Valor da compra dinâmico</li>
+                  <li>Api para consulta de Cep</li>
+                  <li>Alteração de quantidades pelo item ou pelo carrinho</li>
+                  <li>Modal com informações nutricionais</li>
+                  <li>Contagem de avaliações</li>
+                  <li>Média das avaliações por item</li>
+                </ul>
+              </tr>
+
             </div>
-            <div className='objetivos'>
-            <h5><strong>Objetivos do FrioLil's</strong></h5>
-            <p>O objetivo do FrioLil's foi construir um e-commerce moderno, eficiente e
-              escalável, que atenda às demandas dos usuários, garantindo uma
-              experiência ágil e responsiva, e que principalmente, trouxesse uma
-              nova tela para as vendas dos produtos dos nossos patrocinadores</p>
+
+            <div className="vertical-line"></div>
+
+            <div className='tolls-container'>
+            <h2>Ferramentas</h2>
+            <tr className='obj-tools'>
+              <ul className='tools'>
+                <li><img src={iVscode} alt='icone da marca' className='icone'/>{' VSCode'} </li >
+                <li><img src={iNode} alt='icone da marca' className='icone'/>{' Node'} </li >
+                <li><img src={iReact} alt='icone da marca' className='icone'/>{' React'} </li >
+                <li><img src={iBootstrap} alt='icone da marca' className='icone'/>{' Bootstrap'} </li >
+                <li><img src={iGpt} alt='icone da marca' className='icone'/>{' ChatGPT'} </li >
+                <li><img src={iTrello} alt='icone da marca' className='icone'/>{' Trello'} </li >
+                <li><img src={iMeet} alt='icone da marca' className='icone'/>{'iMeet'} </li >
+                <li><img src={iDiscord} alt='icone da marca' className='icone'/>{' Discord'} </li >
+                <li><img src={iGit} alt='icone da marca' className="icone" />{' GitHub'} </li >
+                <li><img src={iPer} alt='icone da marca' className='icone'/>{' Perplexity'} </li >
+                <li><img src={iFont} alt='icone da marca' className='icone'/>{' Font Awesome '} </li >
+                <li><img src={iW3} alt='icone da marca' className='icone'/>{' W3 '} </li >
+                <li><img src={iCafe} alt='icone da marca' className='icone'/>{' Muito café'} </li >
+              </ul>
+            </tr>
             </div>
           </div>
-        </div>
 
+        </div>
+        <div className="vertical-line"></div>
+        <hc />
         <div className='patrocinio-container'>
         <h5><strong>Patrocinadores:</strong></h5>
-        <div>
+        <div className='icon-patrocinadores'>
           <img src={logo1} alt="IBRF" />
           <img src={logo2} alt="IBRF" />
           <img src={logo3} alt="IBRF" />
